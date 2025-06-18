184d583e5cc8ff9a51fcfbd4978e1f3e90663f52
EllieIvy <eleanor.ivy@gstt.nhs.uk>
2025-06-18 17:57:59 +0100

    Update CrossStitchGenerator.py

--- CrossStitchGenerator.py
+++ CrossStitchGenerator.py
@@ -9,23 +9,21 @@ from PIL import Image
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-
-# User to select image path
 def get_image_path():
     root = Tk()
-    root.withdraw()
+    root.withdraw()  # Hide the main window
     file_path = askopenfilename(
         title="Select an image",
         filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.bmp")]
     )
-    return file_path
-
-
-image_path = get_image_path()
-if image_path:
-    print(f"Selected file: {image_path}")
-else:
-    print("No file selected.")
+    return file_path    
+    
+if __name__ == "__main__":
+    image_path = get_image_path()
+    if image_path:
+        print(f"Selected file: {image_path}")
+    else:
+        print("No file selected.")
     
 img = Image.open(image_path)
-img.show()
+img.show()  # just to verify it loaded correctly
